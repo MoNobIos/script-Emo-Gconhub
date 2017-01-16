235082687cf292dfdf42b6e8fce5e2981fbab928
MoNobIos <monoblos_gconsole@hotmail.com>
2017-01-16 15:30:10 +0700

    Update EmoGcon.user.js

--- EmoGcon.user.js
+++ EmoGcon.user.js
@@ -1,14 +1,3 @@
-// ==UserScript==
-// @name        EmoGcon
-// @namespace   gconhub.com
-// @include     http://gconhub.com/*
-// @include     http://www.gconhub.com/*
-// @version     2.0.8
-// @downloadURL https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.user.js
-// @updateURL   https://github.com/b4lmung/EmoGcon/raw/master/EmoGcon.meta.js
-// @grant       none
-// ==/UserScript==
-
 
 function clickKanade(zEvent) {
   //insert('hello world');
